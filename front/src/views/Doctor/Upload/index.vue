e6283728b8bef57bf769e7442c6cf31ab502a464
charlot0316 <2280583075@qq.com>
2021-10-24 19:33:52 +0800

    图片预览基本完善

--- front/src/views/Doctor/Upload/index.vue
+++ front/src/views/Doctor/Upload/index.vue
@@ -17,8 +17,8 @@
       </el-upload>
       <el-button
         type="success"
-        @click="download"
-      >下载结果</el-button>
+        @click="downloadAllImage()"
+      >下载所有图片</el-button>
       <div
         v-for="singleImage in imageList"
         :key="singleImage"
@@ -28,17 +28,18 @@
           <template #header>
             <div class="card-header">
               <span>{{singleImage.name}}</span>
-              <el-button @click="download">下载全部图片</el-button>
+              <el-button @click="downloadASetOfImage(singleImage)">下载全部图片</el-button>
             </div>
           </template>
           <el-row>
             <el-col :span="8">
 
               <el-card :body-style="{ padding: '0px' }">
-                <img
+                <el-image
                   style="width: 100%;"
                   :src="singleImage.origin"
                   class="image"
+                  :preview-src-list="[singleImage.origin,singleImage.bytemap,singleImage.promap]"
                 />
                 <div style="padding: 14px">
                   <span>原始图片</span>
@@ -46,6 +47,7 @@
                     <el-button
                       type="text"
                       class="button"
+                      @click="downloadIamge(singleImage.promap, singleImage.name+'_origin')"
                     >下载图片</el-button>
                   </div>
                 </div>
@@ -55,10 +57,11 @@
             <el-col :span="8">
 
               <el-card :body-style="{ padding: '0px' }">
-                <img
+                <el-image
                   style="width: 100%;"
                   :src="singleImage.bytemap"
                   class="image"
+                  :preview-src-list="[singleImage.bytemap,singleImage.promap,singleImage.origin]"
                 />
                 <div style="padding: 14px">
                   <span>bytemap</span>
@@ -66,6 +69,7 @@
                     <el-button
                       type="text"
                       class="button"
+                      @click="downloadIamge(singleImage.promap, singleImage.name+'_bytemap')"
                     >下载图片</el-button>
                   </div>
                 </div>
@@ -75,10 +79,11 @@
             <el-col :span="8">
 
               <el-card :body-style="{ padding: '0px' }">
-                <img
+                <el-image
                   style="width: 100%;"
                   :src="singleImage.promap"
                   class="image"
+                  :preview-src-list="[singleImage.promap,singleImage.origin,singleImage.bytemap]"
                 />
                 <div style="padding: 14px">
                   <span>promap</span>
@@ -86,6 +91,7 @@
                     <el-button
                       type="text"
                       class="button"
+                      @click="downloadIamge(singleImage.promap, singleImage.name+'_promap')"
                     >下载图片</el-button>
                   </div>
                 </div>
@@ -101,6 +107,7 @@
 </template>
 
 <script>
+
 export default {
   name: "Upload",
   data() {
@@ -116,6 +123,36 @@ export default {
     this.getImageList();
   },
   methods: {
+    downloadAllImage() {
+      var i, len;
+      for (i = 0, len = this.imageList.length; i < len; i++) {
+        this.downloadASetOfImage(this.imageList[i])
+      }
+    },
+    downloadASetOfImage(singleImage) {
+      this.downloadIamge(singleImage.origin, singleImage.name + "_origin")
+      this.downloadIamge(singleImage.bytemap, singleImage.name + "_bytemap")
+      this.downloadIamge(singleImage.promap, singleImage.name + "_promap")
+    },
+    downloadIamge(imgsrc, name) {//下载图片地址和图片名
+      let image = new Image();
+      // 解决跨域 Canvas 污染问题
+      image.setAttribute("crossOrigin", "anonymous");
+      image.onload = function () {
+        let canvas = document.createElement("canvas");
+        canvas.width = image.width;
+        canvas.height = image.height;
+        let context = canvas.getContext("2d");
+        context.drawImage(image, 0, 0, image.width, image.height);
+        let url = canvas.toDataURL("image/png"); //得到图片的base64编码数据
+        let a = document.createElement("a"); // 生成一个a元素
+        let event = new MouseEvent("click"); // 创建一个单击事件
+        a.download = name || "photo"; // 设置图片名称
+        a.href = url; // 将生成的URL设置为a.href属性
+        a.dispatchEvent(event); // 触发a的单击事件
+      };
+      image.src = imgsrc;
+    },
     getImageList() {
       this.imageList = [
         {
@@ -125,7 +162,7 @@ export default {
           promap: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201705%2F20%2F20170520200604_txGEW.thumb.700_0.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1637657441&t=ed45db0968503fa01e0359a76e2b6970",
         },
         {
-          name: "一个图片",
+          name: "又一个图片",
           origin: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2Ff8107c953b8e05b262b2bab62c44acbcb238a7b79ece0-Deltbt_fw658&refer=http%3A%2F%2Fhbimg.b0.upaiyun.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1637657441&t=0d1d0ae8ae31dd257cfb6c985390351a",
           bytemap: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fc-ssl.duitang.com%2Fuploads%2Fitem%2F201705%2F20%2F20170520200616_HN2jR.thumb.1000_0.jpeg&refer=http%3A%2F%2Fc-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1637657441&t=bdd770537b2bff7e58737481c3bb331e",
           promap: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201705%2F20%2F20170520200604_txGEW.thumb.700_0.jpeg&refer=http%3A%2F%2Fb-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1637657441&t=ed45db0968503fa01e0359a76e2b6970",
@@ -160,39 +197,39 @@ export default {
     //       console.log(err);
     //     });
     // },
-    download() {
-      this.$http.post("/download/", this.form.pic_title, {
-        responseType: "blob",
-      })
-        .then((res) => {
-          // console.log(res);
-          if (res.data.success === false) {
-            this.$message.error("出现错误");
-            return;
-          }
-          console.log(res, '看一下res是啥');
-          const data = res.data;
-          const url = window.URL.createObjectURL(
-            new Blob([data], {
-              type: "application/octet-stream",
-            })
-          );
-          console.log(url, '看一下url是啥')
-          console.log("**********");
-          console.log(res.headers['content-disposition']);
-          let fileName = 'test.png'
-          const a = document.createElement("a");
-          document.body.appendChild(a);
-          a.style.display = "none";
-          a.href = url;
-          let filename = fileName;
-          a.download = filename;
-          console.log(filename)
-          a.click();
-          document.body.removeChild(a);
-          window.URL.revokeObjectURL(url);
-        })
-    },
+    // download() {
+    //   this.$http.post("/download/", this.form.pic_title, {
+    //     responseType: "blob",
+    //   })
+    //     .then((res) => {
+    //       // console.log(res);
+    //       if (res.data.success === false) {
+    //         this.$message.error("出现错误");
+    //         return;
+    //       }
+    //       console.log(res, '看一下res是啥');
+    //       const data = res.data;
+    //       const url = window.URL.createObjectURL(
+    //         new Blob([data], {
+    //           type: "application/octet-stream",
+    //         })
+    //       );
+    //       console.log(url, '看一下url是啥')
+    //       console.log("**********");
+    //       console.log(res.headers['content-disposition']);
+    //       let fileName = 'test.png'
+    //       const a = document.createElement("a");
+    //       document.body.appendChild(a);
+    //       a.style.display = "none";
+    //       a.href = url;
+    //       let filename = fileName;
+    //       a.download = filename;
+    //       console.log(filename)
+    //       a.click();
+    //       document.body.removeChild(a);
+    //       window.URL.revokeObjectURL(url);
+    //     })
+    // },
   },
 };
 </script>
