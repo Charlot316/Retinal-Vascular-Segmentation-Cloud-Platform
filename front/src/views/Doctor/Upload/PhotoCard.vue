8dee2faf571b7660a7d72c8c6d435642d35eae14
Charlot316 <2280583075@qq.com>
2022-03-20 10:39:52 +0800

    修改

--- front/src/views/Doctor/Upload/PhotoCard.vue
+++ front/src/views/Doctor/Upload/PhotoCard.vue
@@ -52,156 +52,33 @@
     <!-- 三张图片 -->
     <el-row>
       <el-col :span="8">
-        <el-card :body-style="{ padding: '0px' }">
-          <el-image
-            lazy
-            style="width: 100%;"
-            :src="singleImage.photo_origin"
-            class="image"
-            :preview-src-list="[
-              singleImage.photo_origin,
-              singleImage.photo_upload,
-              singleImage.photo_promap,
-            ]"
-          >
-            <!-- 图片加载出错的情况 -->
-            <template #error>
-              <div class="image-slot">
-                <el-image
-                  lazy
-                  style="width: 100%;"
-                  :src="require('../../../assets/img/loading.gif')"
-                  class="image"
-                />
-                <div style="text-align:center;">
-                  <h3 style="text-align:center">
-                    图片加载中，请稍后刷新
-                  </h3>
-                  <el-button @click="getImageList">强制刷新</el-button>
-                </div>
-              </div>
-            </template>
-          </el-image>
-          <div style="padding: 14px">
-            <span>原始图片</span>
-            <div class="bottom">
-              <el-button
-                type="text"
-                class="button"
-                @click="
-                  downloadIamge(
-                    singleImage.photo_origin,
-                    singleImage.photo_savename + '_origin'
-                  )
-                "
-                >下载图片</el-button
-              >
-            </div>
-          </div>
-        </el-card>
+        <single-photo
+          :singleImage="singleImage"
+          index="origin"
+        />
       </el-col>
       <el-col :span="8">
-        <el-card :body-style="{ padding: '0px' }">
-          <el-image
-            lazy
-            style="width: 100%;"
-            :src="singleImage.photo_upload"
-            class="image"
-            :preview-src-list="[
-              singleImage.photo_upload,
-              singleImage.photo_promap,
-              singleImage.photo_origin,
-            ]"
-          >
-            <template #error>
-              <div class="image-slot">
-                <el-image
-                  style="width: 100%;"
-                  :src="require('../../../assets/img/loading.gif')"
-                  class="image"
-                />
-                <div style="text-align:center;">
-                  <h3 style="text-align:center">
-                    图片加载中，请稍后刷新
-                  </h3>
-                  <el-button @click="getImageList">强制刷新</el-button>
-                </div>
-              </div>
-            </template>
-          </el-image>
-          <div style="padding: 14px">
-            <span>黄金指标</span>
-            <div class="bottom">
-              <el-button
-                type="text"
-                class="button"
-                @click="
-                  downloadIamge(
-                    singleImage.photo_upload,
-                    singleImage.photo + '_upload'
-                  )
-                "
-                >下载图片</el-button
-              >
-            </div>
-          </div>
-        </el-card>
+        <single-photo
+          :singleImage="singleImage"
+          index="upload"
+        />
       </el-col>
       <el-col :span="8">
-        <el-card :body-style="{ padding: '0px' }">
-          <el-image
-            style="width: 100%;"
-            :src="singleImage.photo_promap"
-            class="image"
-            :preview-src-list="[
-              singleImage.photo_promap,
-              singleImage.photo_origin,
-              singleImage.photo_upload,
-            ]"
-          >
-            <template #error>
-              <div class="image-slot">
-                <el-image
-                  style="width: 100%;"
-                  :src="require('../../../assets/img/loading.gif')"
-                  class="image"
-                />
-                <div style="text-align:center;">
-                  <h3 style="text-align:center">
-                    图片加载中，请稍后刷新
-                  </h3>
-                  <el-button @click="getImageList">强制刷新</el-button>
-                </div>
-              </div>
-            </template>
-          </el-image>
-          <div style="padding: 14px">
-            <span>生成图片</span>
-            <div class="bottom">
-              <el-button
-                type="text"
-                class="button"
-                @click="
-                  downloadIamge(
-                    singleImage.photo_promap,
-                    singleImage.photo_savename + '_promap'
-                  )
-                "
-                >下载图片</el-button
-              >
-            </div>
-          </div>
-        </el-card>
+        <single-photo
+          :singleImage="singleImage"
+          index="promap"
+        />
       </el-col>
     </el-row>
   </el-card>
 </template>
 <script>
 import { ElMessageBox, ElMessage } from "element-plus";
+import SinglePhoto from "./SinglePhoto.vue";
 export default {
-  
-  props: ["props","singleImage"],
+  props: ["props", "singleImage"],
   components: {
+    SinglePhoto,
   },
   data() {
     return {
@@ -209,6 +86,40 @@ export default {
     };
   },
   methods: {
+    downloadASetOfImage(singleImage) {
+      this.downloadIamge(
+        singleImage.photo_origin,
+        singleImage.photo_realname + "_origin"
+      );
+      this.downloadIamge(
+        singleImage.photo_promap,
+        singleImage.photo_realname + "_promap"
+      );
+      this.downloadIamge(
+        singleImage.photo_upload,
+        singleImage.photo_realname + "_bytemap"
+      );
+    },
+    downloadIamge(imgsrc, name) {
+      //下载图片地址和图片名
+      let image = new Image();
+      // 解决跨域 Canvas 污染问题
+      image.setAttribute("crossOrigin", "anonymous");
+      image.onload = function() {
+        let canvas = document.createElement("canvas");
+        canvas.width = image.width;
+        canvas.height = image.height;
+        let context = canvas.getContext("2d");
+        context.drawImage(image, 0, 0, image.width, image.height);
+        let url = canvas.toDataURL("image/png"); //得到图片的base64编码数据
+        let a = document.createElement("a"); // 生成一个a元素
+        let event = new MouseEvent("click"); // 创建一个单击事件
+        a.download = name || "photo"; // 设置图片名称
+        a.href = url; // 将生成的URL设置为a.href属性
+        a.dispatchEvent(event); // 触发a的单击事件
+      };
+      image.src = imgsrc;
+    },
     getImageList() {
       this.$emit("getImageList");
     },
@@ -283,5 +194,3 @@ export default {
   },
 };
 </script>
-
-
---
+++ front/src/views/Doctor/Upload/SinglePhoto.vue
@@ -0,0 +1,114 @@
+<template>
+  <el-card :body-style="{ padding: '0px' }">
+    <el-image
+      lazy
+      style="width: 100%;"
+      :src="src"
+      class="image"
+      :preview-src-list="list"
+    >
+      <!-- 图片加载出错的情况 -->
+      <template #error>
+        <div class="image-slot">
+          <el-image
+            lazy
+            style="width: 100%;"
+            :src="require('../../../assets/img/loading.gif')"
+            class="image"
+          />
+          <div style="text-align:center;">
+            <h3 style="text-align:center">
+              图片加载中，请稍后刷新
+            </h3>
+            <el-button @click="getImageList">强制刷新</el-button>
+          </div>
+        </div>
+      </template>
+    </el-image>
+    <div style="padding: 14px">
+      <span>{{ name }}</span>
+      <div class="bottom">
+        <el-button
+          type="text"
+          class="button"
+          @click="downloadIamge(src, singleImage.photo_savename + type)"
+          >下载图片</el-button
+        >
+      </div>
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {
+  props: ["singleImage", "index"],
+  components: {},
+  data() {
+    return {
+      myProps: this.props,
+      list: [],
+      name: "",
+      type: "",
+      src: "",
+    };
+  },
+  created() {
+    this.initialize();
+  },
+  methods: {
+    initialize() {
+      if (this.index == "origin") {
+        this.list = [
+          this.singleImage.photo_origin,
+          this.singleImage.photo_upload,
+          this.singleImage.photo_promap,
+        ];
+        this.name = "原始图片";
+        this.type = "_origin";
+        this.src = this.singleImage.photo_origin;
+      } else if (this.index == "upload") {
+        this.list = [
+          this.singleImage.photo_upload,
+          this.singleImage.photo_promap,
+          this.singleImage.photo_origin,
+        ];
+        this.name = "上传图片";
+        this.type = "_upload";
+        this.src = this.singleImage.photo_upload;
+      } else {
+        this.list = [
+          this.singleImage.photo_promap,
+          this.singleImage.photo_origin,
+          this.singleImage.photo_upload,
+        ];
+        this.name = "生成图片";
+        this.type = "_promap";
+        this.src = this.singleImage.photo_promap;
+      }
+    },
+    getImageList() {
+      this.$emit("getImageList");
+    },
+    downloadIamge(imgsrc, name) {
+      //下载图片地址和图片名
+      let image = new Image();
+      // 解决跨域 Canvas 污染问题
+      image.setAttribute("crossOrigin", "anonymous");
+      image.onload = function() {
+        let canvas = document.createElement("canvas");
+        canvas.width = image.width;
+        canvas.height = image.height;
+        let context = canvas.getContext("2d");
+        context.drawImage(image, 0, 0, image.width, image.height);
+        let url = canvas.toDataURL("image/png"); //得到图片的base64编码数据
+        let a = document.createElement("a"); // 生成一个a元素
+        let event = new MouseEvent("click"); // 创建一个单击事件
+        a.download = name || "photo"; // 设置图片名称
+        a.href = url; // 将生成的URL设置为a.href属性
+        a.dispatchEvent(event); // 触发a的单击事件
+      };
+      image.src = imgsrc;
+    },
+  },
+};
+</script>
