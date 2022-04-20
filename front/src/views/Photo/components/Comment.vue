0549e15c4707a710de140258f05192416e929a17
Charlot316 <2280583075@qq.com>
2022-04-20 10:25:11 +0800

    删除评论

--- front/src/views/Photo/components/Comment.vue
+++ front/src/views/Photo/components/Comment.vue
@@ -21,6 +21,7 @@
 
 <script>
 import UserIcon from "@/components/Public/Icon.vue";
+import { ElMessageBox, ElMessage } from "element-plus";
 export default {
   props: ["comment"],
   components: {
@@ -35,28 +36,41 @@ export default {
     getPhotoInfo() {
       this.$emit("getPhotoInfo");
     },
-    async deleteComment() {
-      await new Promise((resolve) => {
-        this.$http
-          .post(
-            "/deleteComment/",
-            JSON.stringify({
-              comment_id:this.comment.id,
-              doctor_id: this.$store.state.user_id,
-            })
-          )
-          .then((res) => {
-            {
-              if (res.data.success == true) {
-                this.$message.success("评论删除成功");
-                this.getPhotoInfo();
-              } else {
-                this.$message.error("删除失败");
-              }
-            }
+    deleteComment() {
+      ElMessageBox.confirm("本操作无法撤回，您确认删除该评论吗？", "Warning", {
+        confirmButtonText: "OK",
+        cancelButtonText: "Cancel",
+        type: "warning",
+      })
+        .then(async () => {
+          await new Promise((resolve) => {
+            this.$http
+              .post(
+                "/deleteComment/",
+                JSON.stringify({
+                  comment_id: this.comment.id,
+                  doctor_id: this.$store.state.user_id,
+                })
+              )
+              .then((res) => {
+                {
+                  if (res.data.success == true) {
+                    this.$message.success("评论删除成功");
+                    this.getPhotoInfo();
+                  } else {
+                    this.$message.error("删除失败");
+                  }
+                }
+              });
+            resolve();
+          }).then(() => {});
+        })
+        .catch(() => {
+          ElMessage({
+            type: "info",
+            message: "删除被取消",
           });
-        resolve();
-      }).then(() => {});
+        });
     },
   },
 };
@@ -70,10 +84,9 @@ export default {
 .comment-content {
   display: flex;
   align-items: center;
-  padding:10px;
+  padding: 10px;
 }
 .comment-content .inner {
-  width:90%;
+  width: 90%;
 }
-
 </style>
